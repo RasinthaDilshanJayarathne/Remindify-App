7438b20d37e9298adca4858869cc73e7dd3c16af
rasintha_j <rasintha_j@epiclanka.net>
2024-05-14 14:39:28 +0530

    Security Configuration With JWT

---
+++ src/main/java/com/app/remindifyapp/controller/AuthenticationController.java
@@ -0,0 +1,18 @@
+/**
+ * Author : rasintha_j
+ * Date : 5/14/2024
+ * Time : 2:36 PM
+ * Project Name : remindifyapp
+ */
+
+package com.app.remindifyapp.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+}
